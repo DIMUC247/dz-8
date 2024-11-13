694d4e8cc9f2f8070e5096d8f2f47c1bb85e8ff7
DIMUC247 <dimuch.v09@gmail.com>
2024-11-13 19:00:18 +0100

    fggfg

--- app/routes/pizza.py
+++ app/routes/pizza.py
@@ -1,36 +1,9 @@
-from flask import Blueprint,render_template,request,redirect,flash
+from flask import Blueprint,render_template,request,redirect,flash,url_for
 from app.db.__init import Session,Pizza
 from app.data.password import ADMIN_PASS
 
 
 pizza_route = Blueprint("pizzas", __name__)
-
-
-# @pizza_route.get("/")
-# @pizza_route.post("/")
-# def add_pizza():
-#     block = False
-#     msg = ""
-#     with Session() as session:
-#         if request.method == "post":
-#             name = request.form.get("name")
-#             price = request.form.get("price")
-
-#             if request.form.get("password") == ADMIN_PASS:
-#                 pizza = Pizza(name=name,price=price)
-#                 session.add(pizza)
-#                 session.commit()
-#                 msg = "піццу уcпішно додано"
-#             else:
-#                 block = True
-#         pizzas = session.query(Pizza).all()
-#         context = {
-#             "pizzas": pizzas,
-#             "block": block,
-#             "msg": msg
-#         }
-#         return render_template("pizza.html",**context)
-
 
 @pizza_route.get("/pizzas/")
 @pizza_route.post("/pizzas/")
@@ -42,11 +15,44 @@ def add_pizza():
             pizza = Pizza(name=name,price=price)
 
             if request.form.get("password") == ADMIN_PASS:
-                session.add(name)
+                session.add(pizza)
                 session.commit()
                 flash("Піццу успішно додано")
+                return redirect(url_for("pizzas.index"))
             else:
                 flash("Нeвийшло додати піцу")
 
-        pizza = session.query(Pizza)
-        return render_template("add_pizza.html", pizza=pizza)
+        return render_template("add_pizza.html")
+
+
+@pizza_route.get("/pizza/del/<int:id>/")
+def del_pizza(id):
+    with Session() as session:
+        pizza = session.query(Pizza).where(Pizza.id == id).first()
+        session.delete(pizza)
+        session.commit()
+        return redirect(url_for("pizzas.index"))
+
+@pizza_route.get("/")
+def index():
+     with Session() as session:
+        pizzas = session.query(Pizza).all()
+        return render_template("index.html", pizzas=pizzas)
+
+
+
+@pizza_route.get("/pizza/edit/<int:id>")
+@pizza_route.post("/pizza/edit/<int:id>")
+def edit_pizza(id):
+    with Session() as session:
+        pizza = session.query(Pizza).where(Pizza.id == id).first()
+        if request.method == "POST":
+            name = request.form.get("name")
+            price = request.form.get("price")
+
+            pizza.name = name
+            pizza.price = price
+            session.commit()
+            return redirect(url_for("pizzas.index"))
+
+        return render_template("edit_pizza.html", pizza=pizza)
